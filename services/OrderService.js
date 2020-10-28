a742c0db56a7977bf13dd199ad327e1400502da7
Ankur <ankurnahar.an@gmail.com>
2020-10-28 13:56:47 +0600

    placing order bug fix

--- services/OrderService.js
+++ services/OrderService.js
@@ -11,38 +11,34 @@ const orderService = {
 
               const result = await transaction(Orders.knex(), async (trx) => {
 
-                const result1 = await Orders.query(trx).insert({ userID: orderDetails.userID });
+                const order = await Orders.query(trx).insert({ userID: orderDetails.userID });
 
-                console.log("result1- "+result1.orderID);
+                const orderID = order.orderID;
 
-                const orderID = result1.orderID;
+                await Promise.all(
+                    orderDetails.items.map(item => {
+                        return OrderItems.query(trx).insert({orderID: orderID, itemID: item.itemID, quantity: item.quantity});
+                    })
+                );
 
-                console.log("orderID- "+orderID);
+                const itemArray = await Orders.query(trx).withGraphFetched('item').findOne({ orderID: orderID });
 
-
-
-                //adding items to orderitems table
-               const val = orderDetails.items.map(async (item) => {
-                    const value = await OrderItems.query(trx).insert({orderID: orderID, itemID: item.itemID, quantity: item.quantity});
-                    return value;
-                });
-
-                console.log(val);
-
-                const itemArray = await Orders.relatedQuery('item').where({ orderID: orderID});
-                console.log("itemArray: "+itemArray);
                 let total = 0.0;
                 let cost = 0.0;
-                const mappedArray = itemArray.map((item) => {
+
+                const items = itemArray.item.map((item) => {
                     cost = item.price * item.quantity;
                     item.cost = cost;
                     total += cost;
+                    return item;
                 });
-                return {mappedArray, total: total};
+
+                return {items, total: total};
 
               });
-              console.log(result);
-              return {status: 200,result}
+
+              return {status: 200,result};
+
         } catch(err){
             console.log(err);
             return { status: 500, msg: 'Internal error'  };
